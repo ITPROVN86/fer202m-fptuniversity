54a73b0f2efdb2759595aff719c6537969c4f87e
ITPROVN86 <quangltn3@fe.edu.vn>
2023-10-17 22:18:49 +0700

    Creating your own JSX elements

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './slot2/UIStructure/App';
+import MyComponent from './slot2/create-jsx-elements/MyComponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>
 );
 
